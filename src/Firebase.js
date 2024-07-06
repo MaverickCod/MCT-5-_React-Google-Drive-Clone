aa2141087ca7d37e8da37fd9d9b8dddaa480f0b3
MavericCod <921chavanprem@gmail.com>
2024-07-06 22:11:05 +0530

    added api key in .env

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6rtc_yt4fIL-cvNJzRTJHMf01kKNlWls",
-  authDomain: "drive-clone-pgc.firebaseapp.com",
-  projectId: "drive-clone-pgc",
-  storageBucket: "drive-clone-pgc.appspot.com",
-  messagingSenderId: "128163260209",
-  appId: "1:128163260209:web:9e8c2d5392564a7f347210"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
